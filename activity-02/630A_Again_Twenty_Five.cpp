2352de8691db3b67eb5db1f5973d8c25f63d54b6
Isaac Santana <isaacsantanadevr@gmail.com>
2026-06-05 10:53:17 -0300

    Add 630A_Again_Twenty_Five.cpp with main function

---
+++ activity-02/630A_Again_Twenty_Five.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n = 0; cin >> n;
+    if(n == 0) cout << 0;
+    else if(n == 1) cout << 5;
+    else cout << 25;
+
+}
